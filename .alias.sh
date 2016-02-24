c8280fbcc9c10de132e4ab05c7c658d10245ba04
Rogério Zambon <rogeriozambon@gmail.com>
2016-02-24 13:20:55 -0300

    optimizing function

--- .alias.sh
+++ .alias.sh
@@ -5,11 +5,5 @@ alias fu="sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
 alias fm="cat /proc/meminfo"
 
 function wt {
-  if [ -z $1 ]; then
-    CITY='maringa'
-  else
-    CITY=$1
-  fi
-
-  curl --silent --insecure -- wttr.in/$CITY | head -7
+  curl --silent --insecure -- wttr.in/${@:-maringa} | head -7
 }
