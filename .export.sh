f5c8f086384a0a21ad5d8adbe6ab050acb552ecf
Rogério Zambon <rogeriozambon@gmail.com>
2018-02-07 20:47:41 -0200

    Update .export.sh

--- .export.sh
+++ .export.sh
@@ -1,13 +1,7 @@
-export EDITOR="atom"
-export GEM_EDITOR=$EDITOR
-export CLICOLOR="auto"
-export LSCOLORS=ExFxCxDxBxegedabagacad
+export GOROOT=/usr/local/go
+export GOPATH=/home/rogerio/go
 
-export ANDROID_HOME=/home/rrz/Android/Sdk
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export PATH=$PATH:$GOROOT/bin
+export PATH=$PATH:$GOPATH/bin
 
-export PATH=$PATH:/usr/local/ruby/2.3.0/bin
-export PATH=$PATH:/usr/local/node/4.2.4/bin
-export PATH=$PATH:/usr/local/go/1.5.3/bin
-export PATH=$PATH:/usr/local/heroku/bin
-export PATH=$PATH:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools
+export vblank_mode=0
