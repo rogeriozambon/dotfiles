aac6ea84f1a2961fc217c97927baa9157e0b0388
Rogério Zambon <rogeriozambon@gmail.com>
2018-02-07 20:48:27 -0200

    Update .alias.sh

--- .alias.sh
+++ .alias.sh
@@ -1,9 +1,8 @@
-alias ll="ls -Glahs"
+alias vim="code --new-window --wait"
+alias ll="ls -Glahs --color=always"
 alias ds="sudo find ~ -name '.DS_Store' -exec rm {} \;"
-alias st="atom"
-alias fu="sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
-alias fm="cat /proc/meminfo"
+alias fu="sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
 
 function wt {
-  curl --silent --insecure -- wttr.in/${@:-maringa} | head -7
+  curl --silent -- wttr.in/${@:-maringa} | head -7
 }
