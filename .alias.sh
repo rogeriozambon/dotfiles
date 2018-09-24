ce3354de959ce7e81d4abe7471cd38bc6256e411
Rogério Zambon <rogeriozambon@gmail.com>
2018-09-24 14:46:28 -0300

    some changes

--- .alias.sh
+++ .alias.sh
@@ -1,4 +1,6 @@
+alias g="git"
 alias vim="code --new-window --wait"
 alias ll="ls -Glahs --color=always"
 alias ds="sudo find ~ -name '.DS_Store' -exec rm {} \;"
 alias fu="sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
+alias pcat="pygmentize -f terminal256 -O style=native -g"
--- .export.sh
+++ .export.sh
@@ -1,8 +1,11 @@
-export GOROOT=/usr/local/go/1.9.4
+export GOROOT=/usr/local/go/1.11
 export GOPATH=$HOME/go
 
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$HOME/.cargo/bin
+export PATH=$PATH:$HOME/Downloads/poppler-0.52.0/build/utils
+
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre/bin/java
 
 export vblank_mode=0
