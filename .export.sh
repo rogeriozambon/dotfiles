b1fd1a3bc1e501dfae7b5d5756b13153e144ad0c
Rogério Zambon <rogeriozambon@gmail.com>
2019-11-10 15:42:49 -0300

    Update .export.sh

--- .export.sh
+++ .export.sh
@@ -1,11 +1,23 @@
-export GOROOT=/usr/local/go/1.11
-export GOPATH=$HOME/go
+export GOROOT=/usr/local/go/1.13.4
+export GOPATH=$HOME/work/go
+
+export PYENV_ROOT=$HOME/.pyenv
+export PYENV_HOME=$HOME/.pyenv
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
+
+export PIPENV_VERBOSITY=-1
+export PIPENV_IGNORE_VIRTUALENVS=1
+export PIPENV_PYTHON=$PYENV_ROOT/shims/python
+
+export WORKON_HOME=$HOME/.envs
+export PROJECT_HOME=$HOME/work/python
 
 export PATH=$PATH:$GOROOT/bin
 export PATH=$PATH:$GOPATH/bin
+export PATH=$PATH:$PYENV_ROOT/bin
 export PATH=$PATH:$HOME/.cargo/bin
-export PATH=$PATH:$HOME/Downloads/poppler-0.52.0/build/utils
-
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle/jre/bin/java
 
 export vblank_mode=0
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
