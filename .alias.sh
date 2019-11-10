4eb930dc2b06a5d098dc9639d2dcf38c47ee5435
Rogério Zambon <rogeriozambon@gmail.com>
2019-11-10 15:41:59 -0300

    Update .alias.sh

--- .alias.sh
+++ .alias.sh
@@ -1,5 +1,4 @@
 alias g="git"
-alias vim="code --new-window --wait"
 alias ll="ls -Glahs --color=always"
 alias ds="sudo find ~ -name '.DS_Store' -exec rm {} \;"
 alias fu="sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches"
